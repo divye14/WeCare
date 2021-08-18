a5edd355e0487c6abe916cc5cb2873b0df3bd6ea
Viren-ringo <dubeyvirendra92@gmail.com>
2021-08-18 20:43:11 +0530

    wecare firstcommit

---
+++ app/src/main/java/com/capgemini/wecare/RegistrationFragment.kt
@@ -0,0 +1,86 @@
+package com.capgemini.wecare
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegistrationFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegistrationFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val docB = view.findViewById<Button>(R.id.docB)
+        val patientB = view.findViewById<Button>(R.id.patientB)
+        val logInText = view.findViewById<TextView>(R.id.logInT)
+
+        docB.setOnClickListener {
+            // navigate user to doctor Registration page
+            findNavController().navigate(R.id.action_registrationFragment_to_doctorRegistrationFragment)
+        }
+
+        patientB.setOnClickListener {
+            // navigate user to patient Registration page
+            findNavController().navigate(R.id.action_registrationFragment_to_patientRegistrationFragment)
+        }
+
+        logInText.setOnClickListener {
+            // navigate user to login page
+            findNavController().navigate(R.id.action_registrationFragment_to_authenticationFragment)
+        }
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment RegistrationFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RegistrationFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
